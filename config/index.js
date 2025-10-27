7afb6a7d0a07c1494033aae456beed3050d88d36
gagatpadi <padiyogaradhana@gmail.com>
2025-10-27 11:53:34 +0700

    ,

---
+++ config/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+const db = require('./models');
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: false,
+    })
+);
+
+app.listen(port, () => {
+    console.log("Server is running on port 3000");
+});
+
+db.sequelize.sync().then((result) => {
+    app
+    .listen(3000, () => {
+        console.log('server started');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
