e95867078a73278b33ea6d3698beb1322b81aa4a
gagatpadi <padiyogaradhana@gmail.com>
2025-10-27 12:19:11 +0700

    index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const db = require('./models');
+const komik = require('./models/komik');
 app.use(express.json());
 app.use(
     express.urlencoded({ 
@@ -20,4 +21,55 @@ db.sequelize.sync()
         })
     })
         .catch((err) => {console.log(err);
-        });
+        
+    });
+
+app.post("/Komik", async(req,res) => {
+    const data = req.body;
+    try{
+        const Komik = await db.Komik.create(data);
+        res.send(Komik);
+    } catch(err){
+        res.send(err);
+    }
+})
+
+app.post("/Komik", async(req,res) => {
+    try{
+        const Komik = await db.Komik.findAll();
+        res.send(Komik);
+    } catch(err){
+        res.send(err);
+    }
+})
+
+app.put("/Komik/:id", async(req,res)=>{
+    const id = req.params.id;
+    const data = req.body;
+
+    try{
+        const Komik = awaitdb.Komik.findByPk(id);
+        if (!Komik){
+            return res.status(404).send({ message: "Komik tidak tersedia"});
+        }
+        await komik.update(data);
+        res.send({ message: "Komik berhasil di update", komik});
+    }catch(err){
+        res.status(500).send(err);
+    }
+})
+
+app.delete("/Komik/:id", async(req,res)=>{
+    const id = req.params.id;
+
+    try{
+        const Komik = awaitdb.Komik.findByPk(id);
+        if (!Komik){
+            return res.status(404).send({ message: "Komik tidak tersedia"});
+        }
+        await komik.destroy();
+        res.send({ message: "Komik berhasil di hapus", komik});
+    }catch(err){
+        res.status(500).send(err);
+    }
+})
